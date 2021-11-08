9db68bf75cf89af985f492cbc5d1fab3755cf84f
douglasromeiro <douglasromeiro320@gmail.com>
2021-11-08 09:00:51 -0300

    Adicionando conexões e funções parcial com o banco de dados

---
+++ src/main/java/financeiro/usuario/UsuarioRN.java
@@ -0,0 +1,44 @@
+package financeiro.usuario;
+
+import java.util.List;
+
+import financeiro.util.DAOFactory;
+
+public class UsuarioRN {
+
+	private UsuarioDAO usuarioDAO;
+
+	public UsuarioRN() {
+		this.usuarioDAO = DAOFactory.criarUsuarioDAO();
+		
+	}
+	
+	public Usuario usuario(Integer codigo) {
+		return this.usuarioDAO.carregar(codigo);
+		
+	}
+	
+	public Usuario buscarPorLogin(String login) {
+		return this.usuarioDAO.buscarPorLogin(login);
+		
+	}
+	
+	public void salvar(Usuario usuário) {
+		Integer codigo = usuário.getCodigo();
+		if(codigo == null || codigo == 0) {
+			this.usuarioDAO.salvar(usuário);
+		}else {
+			this.usuarioDAO.atualizar(usuário);
+		}
+	}
+	
+	public void excluir(Usuario usuario) {
+		this.usuarioDAO.excluir(usuario);
+		
+	}
+	
+	public List<Usuario> listar() {
+		return this.usuarioDAO.listar();
+	}
+	
+}
---
+++ src/main/java/financeiro/util/HibernateUtil.java
@@ -0,0 +1,26 @@
+package financeiro.util;
+
+import org.hibernate.SessionFactory;
+import org.hibernate.cfg.AnnotationConfiguration;
+import org.hibernate.cfg.Configuration;
+
+public class HibernateUtil {
+
+	private static final SessionFactory sessionFactory = buildSessionFactory();
+
+	private static SessionFactory buildSessionFactory() {
+		try {
+			AnnotationConfiguration cfg = new AnnotationConfiguration();
+			cfg.configure("hibernate.cfg.xml");
+			return cfg.buildSessionFactory();
+
+		} catch (Throwable e) {
+			System.out.println("Criação incial do objeto SessionFactory falho. Erro: " + e);
+			throw new ExceptionInInitializerError(e);
+		}
+	}
+
+	public static SessionFactory getSessionFactory() {
+		return sessionFactory;
+	}
+}
